b1e541d6e3c4a8dfbc30275c01016ce377b46be9
JohnMakous <39932037+JohnMakous@users.noreply.github.com>
2024-06-04 10:50:20 +0800

    Update main.py add power function model

--- main.py
+++ main.py
@@ -298,8 +298,6 @@ def inverse_model(event):
 	y3 = pydom["input#y3"][0].value
 	y4 = pydom["input#y4"][0].value
 
-	c0 = pydom["input#ic"][0].value
-
 	if pydom["input#ic"][0].value != "":
 		c0 = pydom["input#ic"][0].value
 		c0 = float(c0)
@@ -409,3 +407,79 @@ def sqrt_model(event):
 
 	plt.close('all')
 	display(fig1, target='graph', append=False)
+
+def power_model(event):
+	if pydom["input#xmin"][0].value != "":
+		x_min = pydom["input#xmin"][0].value
+		x_min = float(x_min)
+	else:
+		x_min = 0
+	
+	if pydom["input#xmax"][0].value != "":
+		x_max = pydom["input#xmax"][0].value
+		x_max = float(x_max)
+	else:
+		x_max = 10
+
+	if pydom["input#ymin"][0].value != "":
+		y_min = pydom["input#ymin"][0].value
+		y_min = float(y_min)
+	else:
+		y_min = 0
+	
+	if pydom["input#ymax"][0].value != "":
+		y_max = pydom["input#ymax"][0].value
+		y_max= float(y_max)
+	else:
+		y_max = 10
+	
+	x_label = pydom["input#xlabel"][0].value
+	y_label = pydom["input#ylabel"][0].value
+	x1 = pydom["input#x1"][0].value
+	x2 = pydom["input#x2"][0].value
+	x3 = pydom["input#x3"][0].value
+	x4 = pydom["input#x4"][0].value
+
+	y1 = pydom["input#y1"][0].value
+	y2 = pydom["input#y2"][0].value
+	y3 = pydom["input#y3"][0].value
+	y4 = pydom["input#y4"][0].value
+
+	if pydom["input#powerc"][0].value != "":
+		c0 = pydom["input#powerc"][0].value
+	else:
+		c0 = 0
+
+	if pydom["input#powern"][0].value != "":
+		power_n = pydom["input#powern"][0].value
+	else:
+		power_n = 0
+
+	c0float=float(c0)
+	power_n_float = float(power_n)
+	
+	x = np.array([x1,x2,x3,x4])
+	y = np.array([y1,y2,y3,y4])
+	
+	xfloat = [float(i) for i in x]
+	yfloat = [float(i) for i in y]
+		
+	x_model = np.arange(0.0,x_max,.001)
+	y_model = c0float*x_model**power_n_float
+
+	fig1, ax1 = plt.subplots(1, dpi=150, figsize=(3,2.5))
+	ax1.scatter(xfloat,yfloat, 10)
+	plt.plot(x_model, y_model)
+	plt.title(y_label + " vs. "+ x_label, fontsize=6)
+	ax1.set_xlabel(x_label, fontsize=6, labelpad=1)
+	ax1.set_ylabel(y_label, fontsize=6, labelpad=1)
+	ax1.set_xlim(x_min, x_max)
+	ax1.set_ylim(y_min, y_max)
+	ax1.tick_params(axis='x', labelsize=4)
+	ax1.tick_params(axis='y', labelsize=4)
+	ax1.margins(1)
+	ax1.grid()
+
+	plt.close('all')
+	display(fig1, target='graph', append=False)
+
