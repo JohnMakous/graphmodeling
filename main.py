85d8f3b40fea33933802eea489e4f150c30da906
JohnMakous <39932037+JohnMakous@users.noreply.github.com>
2024-08-10 16:16:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -55,10 +55,10 @@ def update_graph(event):
 	else:
 		y_label = "y"
 
-	if pydom["input#graphnote"][0].value != "":
-		graph_note = pydom["input#graphnote"][0].value
-	else:
-		graph_note = "note"
+	#if pydom["input#graphnote"][0].value != "":
+	#	graph_note = pydom["input#graphnote"][0].value
+	#else:
+	#	graph_note = ""
 
 	if pydom["input#graphscale"][0].value != "":
 		graph_scale = pydom["input#graphscale"][0].value
